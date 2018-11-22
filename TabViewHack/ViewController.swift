e3a92a370e232d2d58daf74a096a55b11aa161b3
Vincent Goay <vincentgoay@gmail.com>
2018-11-22 15:49:01 +0800

    Initial Commit

---
+++ TabViewHack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabViewHack
+//
+//  Created by Vincent Goay on 22/11/2018.
+//  Copyright © 2018 Bananasoft IT Solution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
